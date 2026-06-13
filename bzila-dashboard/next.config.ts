97cde99c427610d5f7d4c0225d50de1c2f58b72b
bzila <bzilatrades@gmail.com>
2026-06-12 21:21:53 -0400

    Fix Next.js config outputFileTracingRoot

--- bzila-dashboard/next.config.ts
+++ bzila-dashboard/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sql.js"],
-  outputFileTracingRoot: require("path").join(__dirname, "../../"),
 };
 
 export default nextConfig;
